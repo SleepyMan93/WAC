fb1538c40c92cff31e929fa49c80fc8e1072c21f
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-03-07 17:55:36 +0000

    Crated basic scrolling text panel

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,22 @@ function reveal() {
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 100;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+  function sticky() {
+    var sticks = document.querySelectorAll(".sticky");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
   
       if (elementTop < windowHeight - elementVisible) {
