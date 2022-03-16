0218fcbe5cc141cfc09c44e9261edd6338bc735e
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-03-16 19:13:33 +0000

    Added second carousel with working functionality - Creating js for sticky section next

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ for (i = 0; i < maxItems; i++) {
 $slider.addClass('slideshow-left');
 $('.slideshow-left').slick({
   vertical: true,
-  verticalSwiping: true,
+  verticalSwiping: false,
   arrows: false,
   infinite: true,
   dots: false,
@@ -35,28 +35,6 @@ $('.slideshow-left').slick({
     $('.slideshow-right .slider').slick('slickGoTo', maxItems - 1 - nextSlide);
     $('.slideshow-text').slick('slickGoTo', nextSlide);
   }
-}).on("mousewheel", function(event) {
-  event.preventDefault();
-  if (event.deltaX > 0 || event.deltaY < 0) {
-    $(this).slick('slickNext');
-  } else if (event.deltaX < 0 || event.deltaY > 0) {
-    $(this).slick('slickPrev');
-  };
-}).on('mousedown touchstart', function(){
-  dragging = true;
-  tracking = $('.slick-track', $slider).css('transform');
-  tracking = parseInt(tracking.split(',')[5]);
-  rightTracking = $('.slideshow-right .slick-track').css('transform');
-  rightTracking = parseInt(rightTracking.split(',')[5]);
-}).on('mousemove touchmove', function(){
-  if (dragging) {
-    newTracking = $('.slideshow-left .slick-track').css('transform');
-    newTracking = parseInt(newTracking.split(',')[5]);
-    diffTracking = newTracking - tracking;
-    $('.slideshow-right .slick-track').css({'transform': 'matrix(1, 0, 0, 1, 0, ' + (rightTracking - diffTracking) + ')'});
-  }
-}).on('mouseleave touchend mouseup', function(){
-  dragging = false;
 });
 
 $('.slideshow-right .slider').slick({
@@ -76,3 +54,63 @@ $('.slideshow-text').slick({
   speed: 900,
   cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
 });
+
+/* Slide 2 */
+
+var $slider2 = $('.slideshow2 .slider2'),
+  maxItems2 = $('.item2', $slider2).length,
+  dragging = false,
+  tracking2,
+  rightTracking2;
+
+$sliderRight2 = $('.slideshow2').clone().addClass('slideshow-right2').appendTo($('.split-slideshow2'));
+
+rightItems2 = $('.item2', $sliderRight2).toArray();
+reverseItems2 = rightItems2.reverse();
+$('.slider2', $sliderRight2).html('');
+for (i = 0; i < maxItems; i++) {
+  $(reverseItems2[i]).appendTo($('.slider2', $sliderRight2));
+}
+
+$slider2.addClass('slideshow-left2');
+$('.slideshow-left2').slick({
+  vertical: true,
+  verticalSwiping: false,
+  arrows: false,
+  infinite: true,
+  dots: false,
+  autoplay: true,
+  speed: 1000,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
+}).on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+
+  if (currentSlide > nextSlide && nextSlide == 0 && currentSlide == maxItems - 1) {
+    $('.slideshow-right2 .slider2').slick('slickGoTo', -1);
+    $('.slideshow-text2').slick('slickGoTo', maxItems);
+  } else if (currentSlide < nextSlide && currentSlide == 0 && nextSlide == maxItems - 1) {
+    $('.slideshow-right2 .slider2').slick('slickGoTo', maxItems);
+    $('.slideshow-text2').slick('slickGoTo', -1);
+  } else {
+    $('.slideshow-right2 .slider2').slick('slickGoTo', maxItems - 1 - nextSlide);
+    $('.slideshow-text2').slick('slickGoTo', nextSlide);
+  }
+});
+
+$('.slideshow-right2 .slider2').slick({
+  swipe: false,
+  vertical: true,
+  arrows: false,
+  infinite: true,
+  speed: 950,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)',
+  initialSlide: maxItems - 1
+});
+$('.slideshow-text2').slick({
+  swipe: false,
+  vertical: true,
+  arrows: false,
+  infinite: true,
+  speed: 900,
+  cssEase: 'cubic-bezier(0.7, 0, 0.3, 1)'
+});
+
