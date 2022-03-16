eacebe0b225c35b0d397e8519ba714049d14dafd
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-03-16 19:37:46 +0000

    Created js for second sticky carousel and added smaller videos

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,8 +3,6 @@ $(function () {
       var email = 'alex@wacollective.co.uk';
       var subject = 'Test';
       var emailBody = 'Hi Alex,';
-      var attach = 'path';
-      document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
-          "?attach="+attach;
+      document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
     });
   });
