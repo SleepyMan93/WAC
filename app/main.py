7802584ad77ca2639e1c8c15f238163b2ac0de90
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-03-30 15:36:46 +0000

    Believe the server is working now, just need to get jinja to find the templates

--- app/main.py
+++ app/main.py
@@ -1,27 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates/')
+app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     """Renders home page with base template"""
-    return render_template('index.html')
+    return render_template('/templates/index.html')
 
 
 @app.route("/content_page")
 def content_page():
     """Function to render content/work page"""
-    return render_template('content.html')
+    return render_template('/templates/content.html')
 
 
 @app.route("/about_page")
 def about_page():
     """Function to render the about page"""
-    return render_template('about.html')
+    return render_template('/templates/about.html')
 
 
 @app.route("/services_page")
 def services_page():
     """Function to render the services page"""
-    return render_template('services.html')
+    return render_template('/templates/services.html')
