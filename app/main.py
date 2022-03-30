51c9dfb2557437c655bb3d5212118f2bdabd14df
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-03-30 15:44:29 +0000

    Still having issue getting the app to find the template files

--- app/main.py
+++ app/main.py
@@ -1,28 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates/")
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     """Renders home page with base template"""
-    return render_template('/templates/index.html')
+    return render_template('index.html')
 
 
 @app.route("/content_page")
 def content_page():
     """Function to render content/work page"""
-    return render_template('/templates/content.html')
+    return render_template('content.html')
 
 
 @app.route("/about_page")
 def about_page():
     """Function to render the about page"""
-    return render_template('/templates/about.html')
+    return render_template('about.html')
 
 
 @app.route("/services_page")
 def services_page():
     """Function to render the services page"""
-    return render_template('/templates/services.html')
+    return render_template('services.html')
