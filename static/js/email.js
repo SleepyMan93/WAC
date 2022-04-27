1623346684ddd0519694ac814526ddd1c7f967f4
SleepyMan93 <w-donovan@hotmail.co.uk>
2022-04-27 09:09:18 +0000

    Made mailTo javascript function more robust

--- static/js/email.js
+++ static/js/email.js
@@ -1,8 +1,8 @@
-$(function () {
+$(function OpenEmail() {
     $('.SendEmail').click(function (event) {
       var email = 'josh@wacollective.co.uk';
       var subject = 'Website Query';
       var emailBody = 'Hi Josh,';
-      document.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
+      window.location.href = "mailto:"+email+"?subject="+subject+"&body="+emailBody;
     });
   });
